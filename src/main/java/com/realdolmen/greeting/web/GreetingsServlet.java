a34cc809d5f81ccb8aee884db3873f9d8ec060b7
kleendevos <kleen.devos@gmail.com>
2017-07-03 14:19:34 +0200

    opdracht 03/07/2016

--- src/main/java/com/realdolmen/greeting/web/GreetingsServlet.java
+++ src/main/java/com/realdolmen/greeting/web/GreetingsServlet.java
@@ -1,8 +1,15 @@
 package com.realdolmen.greeting.web;
 
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class GreetingsServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+    }
 }
